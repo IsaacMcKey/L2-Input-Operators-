4862492d9e204224afb4fbee12327d355e951203
Isaac <107879343+IsaacMcKey@users.noreply.github.com>
2022-06-20 19:03:46 +0000

    Story

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+userName = input("Enter your name: ")
+print("Hey " + userName)
+
+userGrade = input ("What grade are you in?")
+print("Hey " + userName + ", " + userGrade + " is a lame grade") 
+
+userYear = int(input("What year were you born? "))
+userAge = 2022 - userYear 
+print (str(userAge) + " is so young!")
+'''
+
+# Create your own Madlibs
+# Have at least 5 inputs 
+userName = input("Enter your name: ")
+userFavoriteShooterGame = input("Enter your favorite place at a resturant")
+userFavoriteFood = input("Enter your favorite food")
+userCelebrity = input("What is your favorite celebrity?")
+uservillain = input("What movie villain do you hate the most")
+userResturant = input("What is your favorite resturant?")
+
+print("userName ") + "findes out that his friend" + ("userCelebrity" ) + "was kidnapped by" ("uservillain") + "sadness over took" + ("userName") + "he made his way to his favorite resturant" + ("userFavoriteFood") + 
