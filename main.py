6992fad76db0972ef58dace871bcc8c43812dd20
Isaac <107879343+IsaacMcKey@users.noreply.github.com>
2022-06-20 20:08:32 +0000

    Finished Story Madbit

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ print (str(userAge) + " is so young!")
 # Create your own Madlibs
 # Have at least 5 inputs 
 userName = input("Enter your name: ")
-userFavoriteShooterGame = input("Enter your favorite place at a resturant")
-userFavoriteFood = input("Enter your favorite food")
-userCelebrity = input("What is your favorite celebrity?")
-uservillain = input("What movie villain do you hate the most")
-userResturant = input("What is your favorite resturant?")
+userFavoriteWeapon = input("Enter your favorite weapon:")
+userFavoriteFood = input("Enter your favorite food:")
+userCelebrity = input("What is your favorite celebrity?:")
+uservillain = input("What movie villain do you hate the most:")
+userResturant = input("What is your favorite resturant?:")
 
-print("userName ") + "findes out that his friend" + ("userCelebrity" ) + "was kidnapped by" ("uservillain") + "sadness over took" + ("userName") + "he made his way to his favorite resturant" + ("userFavoriteFood") + 
+print(userName + " finds out that his friend " + userCelebrity + " was kidnapped by " + uservillain + ". Sadness takes over " + userName + ", so he makes his way to his favorite resturant " + 
+userResturant + " to get some " + userFavoriteFood + ". On his way he finds a " + userFavoriteWeapon + ". With this weapon he realizes he can save his friend " + userCelebrity + ". He goes to fight his nemesis " + uservillain + ". It was a very hard fight however but, " + userName + " wins and saves the day " ) 
